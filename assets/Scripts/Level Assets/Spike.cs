3e5a153b9613252231fb60fe7f33298681c36814
JM3035 <Titansrule3035@gmail.com>
2026-06-30 07:25:44 -0400

    Create directional spike objects

---
+++ assets/Scripts/Level Assets/Spike.cs
@@ -0,0 +1,34 @@
+using Godot;
+using GodotPlugins.Game;
+using System;
+
+public partial class Spike : Sprite2D
+{
+    public override void _Ready()
+    {
+        TileMapLayer tileMapForeground = GetTree().Root.GetNode<TileMapLayer>("level/tiles/Foreground");
+
+        Vector2 local = tileMapForeground.ToLocal(GlobalPosition);
+
+        Vector2I cell = tileMapForeground.LocalToMap(local);
+
+        Vector2 snappedWorld = tileMapForeground.ToGlobal(tileMapForeground.MapToLocal(cell));
+
+        Vector2I atlasCoords = GetAtlasCoords();
+
+        tileMapForeground.SetCell(cell, 1, atlasCoords);
+
+        Visible = false;
+    }
+
+    public Vector2I GetAtlasCoords()
+    {
+        Vector2I coords = Vector2I.Zero;
+
+        Vector2 precoords = (Texture as AtlasTexture).Region.Position;
+
+        coords = new((int)precoords.X, (int)precoords.Y);
+
+        return coords / 32;
+    }
+}
