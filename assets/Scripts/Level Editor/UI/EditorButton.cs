43673b624efa5f03d07a91790052764428512b3c
JM3035 <Titansrule3035@gmail.com>
2026-08-01 22:07:45 -0400

    Move quit button to editor menu

--- assets/Scripts/Level Editor/UI/EditorButton.cs
+++ assets/Scripts/Level Editor/UI/EditorButton.cs
@@ -3,5 +3,17 @@ using System;
 
 public partial class EditorButton : ToolBarButton
 {
+    public override void _Ready()
+    {
+        menuButtons[4] = menu.GetNode<Button>("QuitButton/Button");
 
+        menuButtons[4].Pressed += QuitButtonPressed;
+
+        base._Ready();
+    }
+    void QuitButtonPressed()
+    {
+        blockMouse.MouseFilter = MouseFilterEnum.Stop;
+        GetTree().CurrentScene.GetNode<QuitMenu>("CanvasLayer/UI/QuitMenu").ShowMenu();
+    }
 }
