84ff0066fc7310e698a2be91760694f819a48e02
JM3035 <Titansrule3035@gmail.com>
2026-08-01 22:06:17 -0400

    Move camera drag responsibility

--- assets/Scripts/Level Editor/UI/CameraDrag.cs
+++ assets/Scripts/Level Editor/UI/CameraDrag.cs
@@ -1,4 +1,5 @@
 using Godot;
+using static EditorObject;
 
 public partial class CameraDrag : Camera2D
 {
@@ -9,56 +10,130 @@ public partial class CameraDrag : Camera2D
     private Vector2 _dragStartMousePos;
     private Vector2 _dragStartCameraPos;
 
-    public override void _Input(InputEvent @event)
+    private Vector2 _storedCameraPos;
+
+    private static CameraDrag instance;
+
+    public bool canDrag = false;
+
+    public override void _Ready()
     {
-        if (@event is InputEventMouseButton mouseButton)
-        {
-            if (mouseButton.ButtonIndex == MouseButton.Middle)
-            {
-                if (mouseButton.Pressed)
-                {
-                    _dragging = true;
-                    _dragStartMousePos = GetViewport().GetMousePosition();
-                    _dragStartCameraPos = Position;
-                }
-                else
-                {
-                    _dragging = false;
-                }
-            }
-        }
+        instance = this;
 
-        if (@event is InputEventMouseMotion && _dragging)
-        {
-            Vector2 currentMousePos = GetViewport().GetMousePosition();
-            Vector2 delta = (currentMousePos - _dragStartMousePos) * Zoom;
-            Position = _dragStartCameraPos - delta * DragSpeed;
-        }
+        base._Ready();
     }
-
     public override void _Process(double delta)
     {
+        EditorObject editorObject = GetTree().CurrentScene.GetNode<EditorObject>("EditorObject");
+
         Vector2 move = Vector2.Zero;
 
         if (Input.IsKeyPressed(Godot.Key.I))
+        {
             move.Y -= 1;
+        }
 
         if (Input.IsKeyPressed(Godot.Key.K))
+        {
             move.Y += 1;
+        }
 
         if (Input.IsKeyPressed(Godot.Key.J))
+        {
             move.X -= 1;
+        }
 
         if (Input.IsKeyPressed(Godot.Key.L))
+        {
             move.X += 1;
+        }
 
         if (move != Vector2.Zero)
         {
             move = move.Normalized();
             Position += move * MoveSpeed * (float)delta;
+            ClampToBounds();
         }
 
         if (Input.IsKeyPressed(Godot.Key.F))
+        {
             GlobalPosition = new(-224, -400);
+        }
+
+        if (Input.IsActionJustPressed("spacebar") || Input.IsActionJustPressed("mb_middle"))
+        {
+            canDrag = true;
+
+            if (editorObject.currentCursorMode != EditorObject.CursorMode.DragCamera)
+            {
+                editorObject.lastCursorMode = editorObject.currentCursorMode;
+            }
+
+            editorObject.SetCursorMode(EditorObject.CursorMode.DragCamera);
+        }
+
+        if (Input.IsActionJustReleased("spacebar") || Input.IsActionJustReleased("mb_middle"))
+        {
+            canDrag = _dragging = false;
+
+            editorObject.SetCursorMode(editorObject.lastCursorMode);
+        }
+
+        if (canDrag)
+        {
+            _dragStartMousePos = GetViewport().GetMousePosition();
+            _dragStartCameraPos = Position;
+        }
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseButton)
+        {
+            if (((mouseButton.ButtonIndex == MouseButton.Left && mouseButton.Pressed && Input.IsActionPressed("spacebar")) || (mouseButton.ButtonIndex == MouseButton.Middle && mouseButton.Pressed)))
+            {
+                _dragging = true;
+            }
+            else
+            {
+                _dragging = false;
+            }
+        }
+
+        if (@event is InputEventMouseMotion && _dragging && canDrag)
+        {
+            Vector2 currentMousePos = GetViewport().GetMousePosition();
+            Vector2 delta = (currentMousePos - _dragStartMousePos) * Zoom;
+            Position = _dragStartCameraPos - delta * DragSpeed;
+            ClampToBounds();
+        }
+    }
+
+    public void StorePos()
+    {
+        _storedCameraPos = Position;
+    }
+
+    public void RestorePos()
+    {
+        Position = _storedCameraPos;
+    }
+
+    public static CameraDrag GetInstance()
+    {
+        return instance;
+    }
+    private void ClampToBounds()
+    {
+        Vector2 halfView = GetViewportRect().Size * Zoom * 0.5f;
+
+        Position = new Vector2(
+            Mathf.Clamp(Position.X,
+                LimitLeft + halfView.X,
+                LimitRight - halfView.X),
+            Mathf.Clamp(Position.Y,
+                LimitTop + halfView.Y,
+                LimitBottom - halfView.Y)
+        );
     }
 }
