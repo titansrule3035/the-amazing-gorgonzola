7e777e0e464c9108ae32b957ea7bcf73bd6fa18b
JM3035 <Titansrule3035@gmail.com>
2026-08-01 21:48:07 -0400

    Make Grid turn off during "runtime"

--- assets/Scripts/Level Editor/UI/Grid.cs
+++ assets/Scripts/Level Editor/UI/Grid.cs
@@ -4,22 +4,54 @@ public partial class Grid : Node2D
 {
     [Export] public Color lineColor;
 
+    private bool _showGrid = true;
+
+    public override void _Ready()
+    {
+        Main main = GetTree().CurrentScene as Main;
+
+        main.OnGameStarted += () =>
+        {
+            SetGridVisible(false);
+        };
+
+        main.OnGamePaused += () =>
+        {
+            SetGridVisible(true);
+        };
+    }
+
+    public void SetGridVisible(bool visible)
+    {
+        _showGrid = visible;
+        QueueRedraw();
+    }
+
+    public void ToggleGrid()
+    {
+        SetGridVisible(!_showGrid);
+    }
+
     public override void _Process(double delta)
     {
         if (GetTree().Paused)
+        {
             QueueRedraw();
+        }
     }
 
     public override void _Draw()
     {
-        if (!GetTree().Paused)
+        if (!_showGrid || !GetTree().Paused)
+        {
             return;
-
-        TileMapLayer tileMapLayer = GetTree().CurrentScene
-            .GetNode<TileMapLayer>("level/tiles/Foreground");
+        }
+        TileMapLayer tileMapLayer = GetTree().CurrentScene.GetNode<TileMapLayer>("level/tiles/Foreground");
 
         if (tileMapLayer == null)
+        {
             return;
+        }
 
         Camera2D camera = GetViewport().GetCamera2D();
 
@@ -33,22 +65,18 @@ public partial class Grid : Node2D
 
         for (int x = startCell.X; x <= endCell.X + 1; x++)
         {
-            Vector2 a = tileMapLayer.ToGlobal(
-                tileMapLayer.MapToLocal(new Vector2I(x, startCell.Y)) - halfTile);
+            Vector2 a = tileMapLayer.ToGlobal(tileMapLayer.MapToLocal(new Vector2I(x, startCell.Y)) - halfTile);
 
-            Vector2 b = tileMapLayer.ToGlobal(
-                tileMapLayer.MapToLocal(new Vector2I(x, endCell.Y + 1)) - halfTile);
+            Vector2 b = tileMapLayer.ToGlobal(tileMapLayer.MapToLocal(new Vector2I(x, endCell.Y + 1)) - halfTile);
 
             DrawLine(ToLocal(a), ToLocal(b), lineColor);
         }
 
         for (int y = startCell.Y; y <= endCell.Y + 1; y++)
         {
-            Vector2 a = tileMapLayer.ToGlobal(
-                tileMapLayer.MapToLocal(new Vector2I(startCell.X, y)) - halfTile);
+            Vector2 a = tileMapLayer.ToGlobal(tileMapLayer.MapToLocal(new Vector2I(startCell.X, y)) - halfTile);
 
-            Vector2 b = tileMapLayer.ToGlobal(
-                tileMapLayer.MapToLocal(new Vector2I(endCell.X + 1, y)) - halfTile);
+            Vector2 b = tileMapLayer.ToGlobal(tileMapLayer.MapToLocal(new Vector2I(endCell.X + 1, y)) - halfTile);
 
             DrawLine(ToLocal(a), ToLocal(b), lineColor);
         }
@@ -58,7 +86,7 @@ public partial class Grid : Node2D
     {
         if (what == NotificationPaused || what == NotificationUnpaused)
         {
-            QueueRedraw(); // forces immediate update when state changes
+            QueueRedraw();
         }
     }
 }
